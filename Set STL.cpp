feb5b8be211e87c6156bb55749b029829a09f3de
Abid206 <61822329+Abid206@users.noreply.github.com>
2020-10-24 14:33:55 +0600

    file added

---
+++ Set STL.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define op() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+
+int main()
+{
+    op();
+
+    set<int> s;
+    int q;
+    cin>>q;
+
+    while(q--)
+    {
+        int typ,x;
+        cin>>typ>>x;
+
+        if(typ==1) s.insert(x);
+        else if(typ==2) s.erase(x);
+        else
+        {
+            if(s.count(x)==1) cout<< "Yes"<<endl;
+            else cout<< "No"<<endl;
+        }
+    }
+
+    return 0;
+}
+
+
+
+
+
+
